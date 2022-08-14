05691c01c7a66efa5190ac649882157d3f82a475
Andrew-Radchikov <superprobasite@yandex.ru>
2022-08-14 11:16:20 +0300

    solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,35 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.min=min;
+        this.max=max;
     }
 
     guess() {
-
+        if((this.max-this.min) % 2 == 0){
+            return this.min+(this.max-this.min)/2;
+        }
+        else{
+            return this.min+(this.max-this.min+1)/2;
+        }
     }
 
     lower() {
-
+        if((this.max-this.min) % 2 == 0){
+            this.max=this.min+(this.max-this.min)/2;
+        }
+        else{
+        this.max=this.min+(this.max-this.min+1)/2;
+        }
     }
 
     greater() {
-
+        if((this.max-this.min) % 2 == 0){
+            this.min=this.min+(this.max-this.min)/2;
+        }
+        else{
+            this.min=this.min+(this.max-this.min+1)/2;
+        }   
     }
 }
 
